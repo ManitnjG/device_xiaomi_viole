d9ab0e1f52d4b6c9b7b50066b72695d4b6ebcf53
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-05 18:59:21 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,15 +1,15 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2023 The Evolution X Project
+# Copyright (C) 2023 The lineage X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_violet.mk
+    $(LOCAL_DIR)/lineage_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_violet-userdebug \
-    evolution_violet-user \
-    evolution_violet-eng
+    lineage_violet-userdebug \
+    lineage_violet-user \
+    lineage_violet-eng
     
